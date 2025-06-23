59c2d8ea0f1e3b477d3ab45987e31f64409624f0
varun-legend <165490800+varun-legend@users.noreply.github.com>
2025-06-23 14:11:09 +0530

    Create DLNAServer.kt

---
+++ app/src/main/java/com/varunlegend/vplayer/DLNAServer.kt
@@ -0,0 +1,11 @@
+package com.varunlegend.vplayer
+
+import org.fourthline.cling.android.AndroidUpnpService
+import org.fourthline.cling.model.meta.Device
+import org.fourthline.cling.model.meta.Service
+
+class DLNAServer {
+    // TODO: Initialize Cling UPnP service; publish local content
+    fun startService() { /* stub */ }
+    fun stopService() { /* stub */ }
+}
