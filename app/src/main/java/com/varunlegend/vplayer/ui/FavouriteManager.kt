aa11b86c7e0a8d70462ce36eebab68e1716da6c1
varun-legend <165490800+varun-legend@users.noreply.github.com>
2025-06-23 15:02:38 +0530

    Delete app/src/main/java/com/varunlegend/vplayer/ui directory

--- app/src/main/java/com/varunlegend/vplayer/ui/FavouriteManager.kt
+++
@@ -1,7 +0,0 @@
-package com.varunlegend.vplayer.ui
-import android.content.Context
-import android.net.Uri
-object FavoritesManager {
-    fun addFavorite(ctx: Context, uri: Uri) { /* TODO */ }
-    fun getFavorites(ctx: Context): List<Uri> = emptyList()
-}
--- app/src/main/java/com/varunlegend/vplayer/ui/PlaylistManager.kt
+++
@@ -1,7 +0,0 @@
-package com.varunlegend.vplayer.ui
-import android.content.Context
-import android.net.Uri
-object PlaylistManager {
-    fun savePlaylist(ctx: Context, name: String, items: List<Uri>) { /* TODO */ }
-    fun loadPlaylists(ctx: Context): Map<String, List<Uri>> = emptyMap()
-}
