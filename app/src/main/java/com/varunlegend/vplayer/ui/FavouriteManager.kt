3a86210a930186235422aa54075d8415674f7e1d
varun-legend <165490800+varun-legend@users.noreply.github.com>
2025-06-23 13:54:51 +0530

    Create FavouriteManager.kt

---
+++ app/src/main/java/com/varunlegend/vplayer/ui/FavouriteManager.kt
@@ -0,0 +1,7 @@
+package com.varunlegend.vplayer.ui
+import android.content.Context
+import android.net.Uri
+object FavoritesManager {
+    fun addFavorite(ctx: Context, uri: Uri) { /* TODO */ }
+    fun getFavorites(ctx: Context): List<Uri> = emptyList()
+}
