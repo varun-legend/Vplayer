723c065d75a8114837066689c13dbb6c39f5bee0
varun-legend <165490800+varun-legend@users.noreply.github.com>
2025-06-23 14:24:57 +0530

    Update PlayerActivity.kt

--- app/src/main/java/com/varunlegend/vplayer/PlayerActivity.kt
+++ app/src/main/java/com/varunlegend/vplayer/PlayerActivity.kt
@@ -5,71 +5,66 @@ import android.content.res.Configuration
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.MotionEvent
 import androidx.appcompat.app.AppCompatActivity
 import com.varunlegend.vplayer.databinding.ActivityPlayerBinding
-import com.varunlegend.vplayer.utils.HardwareDecoderManager
+import com.varunlegend.vplayer.utils.MediaUtils
 import com.varunlegend.vplayer.utils.SubtitleGestureHelper
 import com.varunlegend.vplayer.utils.ZoomPanListener
 import androidx.media3.common.MediaItem
 import androidx.media3.common.Player
-import androidx.media3.exoplayer.ExoPlayer
-import androidx.media3.session.PlayerNotificationManager
-import androidx.media3.session.MediaSession
-import androidx.media3.ui.PlayerView
 
 class PlayerActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityPlayerBinding
-    private var player: ExoPlayer? = null
+    private lateinit var b: ActivityPlayerBinding
+    private lateinit var player: Player
+    private lateinit var subHelper: SubtitleGestureHelper
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityPlayerBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        b = ActivityPlayerBinding.inflate(layoutInflater)
+        setContentView(b.root)
 
-        initPlayer()
-        setupGestures()
-        setupHardwareDecoderToggle()
-    }
+        val uri = Uri.parse(intent.getStringExtra("uri"))
+        player = MediaUtils.buildPlayer(this)
+        b.playerView.player = player
+        player.setMediaItem(MediaItem.fromUri(uri))
+        player.prepare(); player.playWhenReady = true
 
-    private fun initPlayer() {
-        player = ExoPlayer.Builder(this).build().apply {
-            val uri = Uri.parse(intent.getStringExtra("uri"))
-            setMediaItem(MediaItem.fromUri(uri))
-            playWhenReady = true
-            prepare()
-
-            // Notification + lock-screen
-            PlayerNotificationManager.Builder(
-                this@PlayerActivity, 1, "channel"
-            ).setMediaDescriptionAdapter(MediaNotificationAdapter(this@PlayerActivity, this))
-             .build().setPlayer(this)
-
-            MediaSession.Builder(this@PlayerActivity, this).build()
+        // Gestures: zoom & subtitles
+        val zoomListener = ZoomPanListener(b.playerView)
+        subHelper = SubtitleGestureHelper(this, player)
+        b.playerView.setOnTouchListener { v, e ->
+            zoomListener.onTouch(v, e) || subHelper.onTouch(b.playerView, e)
         }
-        binding.playerView.player = player
     }
 
-    private fun setupGestures() {
-        // Pinch zoom + pan
-        binding.playerView.useController = false
-        binding.playerView.setOnTouchListener(ZoomPanListener())
-        // Subtitle gestures
-        SubtitleGestureHelper(binding.subtitleView, player)
+    override fun onCreateOptionsMenu(menu: Menu) = true.also {
+        menuInflater.inflate(R.menu.player_menu, menu)
     }
 
-    private fun setupHardwareDecoderToggle() {
-        HardwareDecoderManager(this, player!!).attachToggle(binding.btnHwToggle)
+    override fun onOptionsItemSelected(item: MenuItem) = when(item.itemId) {
+        R.id.action_speed -> {
+            MediaUtils.showSpeedDialog(this, player)
+            true
+        }
+        R.id.action_zoom_reset -> {
+            // Reset zoom by resetting matrix
+            ZoomPanListener(b.playerView).apply { /* logic */ }
+            true
+        }
+        else -> super.onOptionsItemSelected(item)
     }
 
     override fun onUserLeaveHint() {
-        super.onUserLeaveHint()
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O && resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+        if (Build.VERSION.SDK_INT >= 26 && resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE) {
             enterPictureInPictureMode(PictureInPictureParams.Builder().build())
         }
     }
 
     override fun onDestroy() {
-        player?.release()
         super.onDestroy()
+        player.release()
     }
 }
